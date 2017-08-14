1aa407a679b90c92f2db26157174446bae093b0f
sapnaban <sriram.padmanaban@onit.com>
2017-08-14 15:24:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ function createtemplate(data){
                     ${date}
                 </div>
                 <div>
-                    ${content}
+                    ${Content}
                 </div>
             </div>
         </body>
