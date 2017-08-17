958268867dbf1eaa8a47bf8b06d34b284d9bdd65
sapnaban <sriram.padmanaban@onit.com>
2017-08-17 11:46:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@ console.log('Loaded!');
 var element = document.getElementById("main-text");
 element.innerHTML = "New Value";
 var marginleft = 0;
-var img = document.getElementById ("img");
+var img = document.getElementById("img");
 function MoveRight(){
     marginleft = marginleft + 1;
     img.style.marginleft = marginleft + 'px';
