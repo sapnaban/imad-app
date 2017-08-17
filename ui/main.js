3cf0269ab8b4d92b3c05360630c883c8f4ec0444
sapnaban <sriram.padmanaban@onit.com>
2017-08-17 11:56:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,13 +1,13 @@
 console.log('Loaded!');
 var element = document.getElementById('main-text');
 element.innerHTML = "New Value";
-var marginleft = 0;
+var marginLeft = 0;
 var img = document.getElementById('madi');
-function MoveRight(){
-    marginleft = marginleft + 1;
-    img.style.marginleft = marginleft + 'px';
+function MoveRight() {
+    marginLeft = marginLeft + 1;
+    img.style.marginLeft = marginLeft + 'px';
     console.log(marginleft);
 }
 img.onclick = function() {
-    var interval = setInterval(MoveRight, 10);
+    var interval = setInterval(MoveRight, 50);
 };
