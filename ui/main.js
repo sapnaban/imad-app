63d3c47440fa7dd7973d7de8159a60fb6154f179
sapnaban <sriram.padmanaban@onit.com>
2017-08-17 11:59:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,7 +6,6 @@ var img = document.getElementById('madi');
 function MoveRight() {
     marginLeft = marginLeft + 1;
     img.style.marginLeft = marginLeft + 'px';
-    console.log(marginleft);
 }
 img.onclick = function() {
     var interval = setInterval(MoveRight, 50);
