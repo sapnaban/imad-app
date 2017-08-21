6787ceef6aab9eeb3c95db0c3f6d3b95dc6ffc34
sapnaban <sriram.padmanaban@onit.com>
2017-08-21 11:42:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -99,12 +99,12 @@ app.get('/', function (req, res) {
 });
 
 app.get('/:articleName', function(req,res){
-    var articleName = req.params.articleName;
-   res.send(createtemplate(articles[articleName]));
+var articleName = req.params.articleName;
+    res.send(createtemplate(articles[articleName]));
 });
 
 var counter = 0;
-app.get('/counter', function(req,res){
+app.get('/Counter', function(req,res){
     counter = counter + 1;
     res.send(counter.toString());
 });
