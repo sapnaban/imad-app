286c34d90b102fde6a892a30be0a944ef362b1f0
sapnaban <sriram.padmanaban@onit.com>
2017-09-07 13:51:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,12 +1,13 @@
-console.log('Loaded!');
-var element = document.getElementById('main-text');
-element.innerHTML = "New Value";
-var marginLeft = 0;
-var img = document.getElementById('img');
-function MoveRight() {
-    marginLeft = marginLeft + 1;
-    img.style.marginLeft = marginLeft + 'px';
-}
-img.onclick = function() {
-    var interval = setInterval(MoveRight, 50);
+// Counter code
+var button = document.getElementById('counter');
+var counter = 0;
+button.onclick=function(){
+  //Make a request to the counter endpoint
+  
+  //capture the response and store it in a variable
+  
+  //render the variable in the correct span
+  counter = counter + 1;
+  var span  = document.getElementById('count');
+  span.innerHTML = counter.toString();
 };
